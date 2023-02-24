7534319768f1e691f5320eb844797e4e50dc08a2
Mirco Heck <66530591+Cremetoertchen0815@users.noreply.github.com>
2023-02-24 15:22:21 +0100

    Improved shit even further

--- Nez.REST/RESTManager.cs
+++ Nez.REST/RESTManager.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework.Graphics;
 using RestSharp;
 using RestSharp.Authenticators;
+using System.Net;
 
 namespace Nez.REST;
 public class RESTManager : GlobalManager
@@ -9,31 +10,85 @@ public class RESTManager : GlobalManager
 
     private Queue<Action> _callbacks = new();
 
+    public delegate void RestDataCallback<T>(T? Data, HttpStatusCode status);
+    public delegate void RestStatusCallback(HttpStatusCode status);
+
     public RESTManager(string URL, IAuthenticator? authenticator = null)
     {
         Client = new RestClient(URL);
         Client.Authenticator = authenticator;
     }
-    
-    public void Get<T>(string resource, Action<T?> callback, bool synchronize = false)
+
+
+    public void Get<T>(string resource, IEnumerable<Parameter>? parameters = null, RestDataCallback<T>? callback = null, bool synchronize = false) =>
+        RESTDataSend(resource, Method.Get, parameters, callback, synchronize);
+
+    public void Post(string resource, IEnumerable<Parameter>? parameters = null, RestStatusCallback? callback = null, bool synchronize = false) =>
+        RESTStatusSend(resource, Method.Post, parameters, callback, synchronize);
+    public void Post<T>(string resource, IEnumerable<Parameter>? parameters = null, RestDataCallback<T>? callback = null, bool synchronize = false) => 
+        RESTDataSend(resource, Method.Post, parameters, callback, synchronize);
+
+
+    public void Put(string resource, IEnumerable<Parameter>? parameters = null, RestStatusCallback? callback = null, bool synchronize = false) =>
+        RESTStatusSend(resource, Method.Put, parameters, callback, synchronize);
+    public void Put<T>(string resource, IEnumerable<Parameter>? parameters = null, RestDataCallback<T>? callback = null, bool synchronize = false) =>
+        RESTDataSend(resource, Method.Put, parameters, callback, synchronize);
+
+
+    public void Delete(string resource, IEnumerable<Parameter>? parameters = null, RestStatusCallback? callback = null, bool synchronize = false) =>
+        RESTStatusSend(resource, Method.Delete, parameters, callback, synchronize);
+    public void Delete<T>(string resource, IEnumerable<Parameter>? parameters = null, RestDataCallback<T>? callback = null, bool synchronize = false) =>
+        RESTDataSend(resource, Method.Delete, parameters, callback, synchronize);
+
+
+    private void RESTDataSend<T>(string resource, Method method, IEnumerable<Parameter>? parameters, RestDataCallback<T>? callback, bool synchronize)
     {
-        _ = ExecuteRESTAsync(new RestRequest(resource, Method.Get), callback, synchronize);
+        var req = new RestRequest(resource, method);
+        if (parameters is not null) req.Parameters.AddParameters(parameters);
+        _ = ExecuteRESTAsync(req, callback, synchronize);
+    }
+
+    private void RESTStatusSend(string resource, Method method, IEnumerable<Parameter>? parameters, RestStatusCallback? callback, bool synchronize)
+    {
+        var req = new RestRequest(resource, method);
+        if (parameters is not null) req.Parameters.AddParameters(parameters);
+        _ = ExecuteRESTAsync(req, callback, synchronize);
+    }
+
+    public void Execute(RestRequest request, RestStatusCallback? callback = null, bool synchronize = false) =>
+        _ = ExecuteRESTAsync(request, callback, synchronize);
+    public void Execute<T>(RestRequest request, RestDataCallback<T>? callback = null, bool synchronize = false) =>
+        _ = ExecuteRESTAsync(request, callback, synchronize);
+
+
+
+    private async Task ExecuteRESTAsync(RestRequest request, RestStatusCallback? callback, bool synchronize)
+    {
+        var result = await Client.ExecuteAsync(request);
+        if (callback == null) return;
+
+        if (synchronize)
+            _callbacks.Enqueue(() => callback(result.StatusCode));
+        else
+            callback(result.StatusCode);
+    }
+
+    private async Task ExecuteRESTAsync<T>(RestRequest request, RestDataCallback<T>? callback, bool synchronize)
+    {
+        var result = await Client.ExecuteAsync<T>(request);
+        if (callback == null) return;
+        
+        if (synchronize)
+            _callbacks.Enqueue(() => callback(result.Data, result.StatusCode));
+        else
+            callback(result.Data, result.StatusCode);
     }
 
     public void StreamTexture(string resource, Action<Texture2D?> callback) =>
         _ = StreamTextureAsync(new RestRequest(resource, Method.Get), callback);
 
-    private async Task ExecuteRESTAsync<T>(RestRequest request, Action<T?> callback, bool synchronize)
-    {
-        var result = await Client.ExecuteGetAsync<T>(request);
-        if (synchronize)
-            _callbacks.Enqueue(() => callback(result.Data));
-        else
-            callback(result.Data);
-    }
     private async Task StreamTextureAsync(RestRequest request, Action<Texture2D?> callback)
     {
-
         var canc = new CancellationToken();
         var result = await Client.DownloadStreamAsync(request, canc);
         callback(result != null ? Texture2D.FromStream(Core.GraphicsDevice, result) : null);
